ffea2179448f1b0ab932cf5220befa1b2801b774
Himanshu Kamboj <himanshukamboj0027@gmail.com>
2026-08-22 21:18:39 +0530

    Update portfolio content

--- my-app/src/components/Content.jsx
+++ my-app/src/components/Content.jsx
@@ -3,7 +3,13 @@ import React from 'react'
 export default function content() {
   return (
     <div>
-       
+       <div className="main">
+          <div className="sub-div">
+            
+          </div>
+          <div className="sub-div"></div>
+          <div className="sub-div"></div>
+       </div>
     </div>
   )
 }
